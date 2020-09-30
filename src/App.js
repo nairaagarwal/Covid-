4364e33336a2198c909e78cdab9b5be79c856746
nairaagarwal <72147814+nairaagarwal@users.noreply.github.com>
2020-09-30 20:19:10 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import CountryList from './components/CountryList/CountryList';
+import SearchBox from './components/SearchBox/SearchBox';
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+     countries:[],
+     stats:[],
+     searchField:''
+    }
+  }
+  async componentDidMount(){
+    const resp = await fetch('https://api.covid19api.com/countries')
+   
+    this.setState({countries})
+    this.state.countries.forEach(async country => {
+      const resp = await fetch(`https://api.covid19api.com/total/country/${country.Slug}`)
+      
+      if(data.length)
+      this.setState(prevState => (
+        {stats:prevState.stats.concat({...data[data.length - 1],CountryCode:country.ISO2})}
+      ))
+    })
+  }
+  handleChange = (e) =>{
+    this.setState({searchField:e.target.value})
+  }
+  render(){
+    const {stats,searchField} = this.state
+    const filteredCountries = stats.filter(country =>(
+      country.Country.toLowerCase().includes(searchField.toLowerCase())
+    ))
+    return(
+      <div className="App">
+        <h1>Covid19 Stats Web App</h1>
+        
+    </div>
+    )
+  }
+}
+
+export default App;
